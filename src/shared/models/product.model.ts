a9435d050993dbee59ec25b755893c6ce04cf519
Colin White <clwhite814@gmail.com>
2024-11-04 14:56:07 -0600

    Added product model

---
+++ src/shared/models/product.model.ts
@@ -0,0 +1,11 @@
+export interface Product {
+    productID: number;
+    name: string;
+    brandID: number;
+    sku: string;
+    category: string;
+    price: number;
+    stockQuantity: number;
+    createdAt: string; 
+  }
+  
